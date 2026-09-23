4093278c14b9413a604b898cd450a3ad1329a54b
TENG,LI-CHANG <62901289+maglili@users.noreply.github.com>
2026-09-23 23:54:20 +0800

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets, reverse=True):
+            adj[src].append(dst)
+
+        res = []
+
+        def dfs(src):
+            while adj[src]:
+                dfs(adj[src].pop())
+            res.append(src)
+
+        dfs("JFK")
+        return res[::-1]
